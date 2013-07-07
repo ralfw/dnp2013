c3583348bf206fa1cfd3f2af6ec945ec77169fb7
Ralf Westphal <info@ralfw.de>
2013-07-07 10:28:30 +0200

    HeckleService ASP.NET App mit Nancy angelegt

---
+++ Heckle Service/appharbor/HeckleService/HeckleService/NancyServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace HeckleService
+{
+    public class NancyServer : NancyModule
+    {
+        public NancyServer()
+        {
+            Get["/"] = x => "hello, world - " + DateTime.Now.ToString();
+        }
+    }
+}
