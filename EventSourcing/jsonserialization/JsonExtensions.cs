1f035d15cdd210f0335474f6629a3dddf897a3f1
Ralf Westphal <info@ralfw.de>
2013-10-05 11:44:14 +0200

    Json (de)serialization and pretty printing

---
+++ EventSourcing/jsonserialization/JsonExtensions.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using ImpromptuInterface;
+
+namespace jsonserialization
+{
+    public static class JsonExtensions
+    {
+        public static object FromJson(this string jsonObj)
+        {
+            return Json.Decode(jsonObj);
+        }
+
+
+        public static string ToJson(this object obj)
+        {
+            var json = ObjectToJson(obj);
+            return PrettifyJson(json);
+        }
+
+        private static string ObjectToJson(object obj)
+        {
+            if (obj.GetType() == typeof(DynamicJsonObject)) return DynamicJsonObjectToJson(obj as DynamicJsonObject);
+            if (!(obj is ExpandoObject)) return Json.Encode(obj);
+
+            var json = "{";
+            var dict = obj as IDictionary<string, object>;
+            var iKey = 0;
+            foreach (var key in dict.Keys)
+            {
+                if (iKey++ > 0) json += ", ";
+                json += "\"" + key + "\": ";
+
+                if (dict[key].GetType() == typeof(DynamicJsonObject))
+                    json += DynamicJsonObjectToJson(dict[key] as DynamicJsonObject);
+                else if (dict[key] is ExpandoObject)
+                    json += ObjectToJson(dict[key]);
+                else
+                    json += Json.Encode(dict[key]);
+            }
+            json += "}";
+            return json;
+        }
+
+        static string DynamicJsonObjectToJson(this DynamicJsonObject obj)
+        {
+            var json = "{";
+            var iName = 0;
+            foreach (var name in obj.GetDynamicMemberNames())
+            {
+                if (iName++ > 0) json += ", ";
+                json += "\"" + name + "\": ";
+
+                object value = Impromptu.InvokeGet(obj, name);
+                json += ObjectToJson(value);
+            }
+            json += "}";
+            return json;
+        }
+
+
+        // source: http://stackoverflow.com/questions/4580397/json-formatter-in-c
+        private const string INDENT_STRING = "  ";
+        private static string PrettifyJson(string str)
+        {
+            var indent = 0;
+            var quoted = false;
+            var sb = new StringBuilder();
+            for (var i = 0; i < str.Length; i++)
+            {
+                var ch = str[i];
+                switch (ch)
+                {
+                    case '{':
+                    case '[':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, ++indent).ToList().ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, --indent).ToList().ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        sb.Append(ch);
+                        break;
+                    case '"':
+                        sb.Append(ch);
+                        bool escaped = false;
+                        var index = i;
+                        while (index > 0 && str[--index] == '\\')
+                            escaped = !escaped;
+                        if (!escaped)
+                            quoted = !quoted;
+                        break;
+                    case ',':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, indent).ToList().ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case ':':
+                        sb.Append(ch);
+                        if (!quoted)
+                            sb.Append(" ");
+                        break;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ EventSourcing/jsonserialization/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.Web.Script.Serialization;
+using ImpromptuInterface;
+
+namespace jsonserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic o;
+            string json;
+            o = "{'name':'value', 'array':['a', 'b', {'baz': 99}], 'obj': {'foo': 42}}".FromJson();
+            Console.WriteLine(o.name);
+            Console.WriteLine(o.array[1]);
+            Console.WriteLine(o.obj.foo);
+
+            Console.WriteLine(JsonExtensions.ToJson(o));
+
+            o = new ExpandoObject();
+            o.name = "value2";
+            o.array = new[] { "a2", "b2" };
+            o.obj = new ExpandoObject();
+            o.obj.foo = 422;
+            json = JsonExtensions.ToJson(o);
+            Console.WriteLine(json);
+
+            var b = new Bar {name="hello", ages=new[]{1, 2, 3}, bar = new Bar{name="world"}};
+            Console.WriteLine(b.ToJson());
+        }
+    }
+
+
+    class Bar
+    {
+        public string name;
+        public int[] ages;
+        public Bar bar;
+    }
+}
