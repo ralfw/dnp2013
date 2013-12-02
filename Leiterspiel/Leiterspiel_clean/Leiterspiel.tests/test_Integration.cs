49cdbddaef0dbdc6d301aa69f17dfd2ce1fbf102
Ralf Westphal <info@ralfw.de>
2013-12-02 13:35:48 +0100

    Integrationstest für Leiterspiel

---
+++ Leiterspiel/Leiterspiel_clean/Leiterspiel.tests/test_Integration.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Leiterspiel.contracts;
+using Leiterspiel.domain;
+using NUnit.Framework;
+
+namespace Leiterspiel.tests
+{
+    [TestFixture]
+    public class test_Integration
+    {
+        [Test]
+        public void UI_with_domain()
+        {
+            var ui = new MockUI();
+            var game = new Game();
+            
+            Program.Integrate(new[]{"leiterspielbrett1.txt"}, ui, game);
+
+            ui.Fire_Started();
+            Assert.AreEqual("5;6;30", ui.Received);
+
+            ui.Fire_Number_of_players_entered();
+            Assert.AreEqual("0;0", ui.Received);
+
+            ui.Fire_Rolled_the_dice();
+            Assert.AreEqual("1;0", ui.Received);
+        }
+
+
+        [Test]
+        public void Domain_with_UI()
+        {
+            var ui = new MockUI();
+            var game = new MockGame();
+
+            Program.Integrate(new[] { "leiterspielbrett1.txt" }, ui, game);
+
+            game.Fire_Game_over();
+            Assert.AreEqual("1", ui.Received);
+        }
+
+
+
+        class MockUI : IUI
+        {
+            public event Action Started;
+            public event Action<int> Number_of_players_entered;
+            public event Action<int> Rolled_the_dice;
+
+
+            public void Fire_Started()
+            {
+                Started();
+            }
+
+            public void Fire_Number_of_players_entered()
+            {
+                Number_of_players_entered(2);
+            }
+
+            public void Fire_Rolled_the_dice()
+            {
+                Rolled_the_dice(3);
+            }
+
+
+            public string Received;
+
+
+            public void Show()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Board_prepared(int number_of_rows, int number_of_cols, int goalIndex)
+            {
+                Received = string.Format("{0};{1};{2}", number_of_rows, number_of_cols, goalIndex);
+            }
+
+            public void Update_player_position(int player, int position)
+            {
+                Received = string.Format("{0};{1}", player, position);
+            }
+
+            public void Game_over(int winning_player)
+            {
+                Received = winning_player.ToString();
+            }
+        }
+
+
+        class MockGame : IGame
+        {
+            public event Action<int, int, int> Initialized;
+            public event Action<int, int> Player_moved;
+            public event Action<int> Game_over;
+
+
+            public void Fire_Game_over()
+            {
+                Game_over(1);
+            }
+
+
+            public void Initialize(Board board)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Set_players(int number_of_players)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Move_player(int number)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- Leiterspiel/Leiterspiel_clean/Program.cs
+++ Leiterspiel/Leiterspiel_clean/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Text;
+using Leiterspiel.contracts;
 using Leiterspiel.domain;
 using Leiterspiel.portals;
 
@@ -8,25 +9,31 @@ namespace Leiterspiel
 {
   class Program
   {
-    static void Main(string[] args)
-    {
+      static void Main(string[] args)
+      {
+          var ui = new UI();
+          var game = new Game();
 
-        var game = new Game();
-        var ui = new UI();
+          Integrate(args, ui, game);
 
-        ui.Started += () => {
-            var boardDefinition = File.ReadAllText(args[0]);
-            var board = Board.Parse(boardDefinition);
-            game.Initialize(board);
-        };
-        game.Initialized += ui.Board_prepared;
+          ui.Show();
+      }
 
-        ui.Number_of_players_entered += game.Set_players;
-        ui.Rolled_the_dice += game.Move_player;
-        game.Player_moved += ui.Update_player_position;
-        game.Game_over += ui.Game_over;
 
-        ui.Show();
-    }
+      internal static void Integrate(string[] args, IUI ui, IGame game)
+      {
+          ui.Started += () =>
+              {
+                  var boardDefinition = File.ReadAllText(args[0]);
+                  var board = Board.Parse(boardDefinition);
+                  game.Initialize(board);
+              };
+          game.Initialized += ui.Board_prepared;
+
+          ui.Number_of_players_entered += game.Set_players;
+          ui.Rolled_the_dice += game.Move_player;
+          game.Player_moved += ui.Update_player_position;
+          game.Game_over += ui.Game_over;
+      }
   }
 }
